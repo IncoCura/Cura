73e811408c932b0aec3badb57278f29bc925e863
KangDroid <hyunwoo286@gmail.com>
2018-12-18 12:39:20 +0900

    Use my theme

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -170,7 +170,7 @@ class CuraApplication(QtApplication):
                          tray_icon_name = "cura-icon-32.png",
                          **kwargs)
 
-        self.default_theme = "cura-light"
+        self.default_theme = "inco-default" # cura-dark, cura-light, inco-default
 
         self.change_log_url = "https://ultimaker.com/ultimaker-cura-latest-features"
 
