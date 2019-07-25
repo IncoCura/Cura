ab100c4e64011f27412eaa776825085bfa93b7f3
KangDroid <hyunwoo286@gmail.com>
2019-07-25 23:14:05 +0900

    Update:
    1. Cura to dark one :)
    2. Change printer profile(CoreM: End GCode, SF/CORE200: Erase Leveling Part

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -171,7 +171,7 @@ class CuraApplication(QtApplication):
                          tray_icon_name = "cura-icon-32.png",
                          **kwargs)
 
-        self.default_theme = "cura-light" # cura-dark, cura-light, inco-default
+        self.default_theme = "cura-dark" # cura-dark, cura-light, inco-default
 
         self.change_log_url = "https://ultimaker.com/ultimaker-cura-latest-features"
 
